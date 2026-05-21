63c1099714a2ebd2e5b843c58ac7e5d1aff5b84a
ebaadwy <ebabawy@email.com>
2026-05-21 13:12:53 +0300

    connect contact form frontend with backend

--- Client/SRC/components/contact/ContactForm.jsx
+++ Client/SRC/components/contact/ContactForm.jsx
@@ -1,39 +1,104 @@
 import styles from "./Contact.module.css";
+import { useState } from "react";
 
 export default function ContactForm() {
+
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await fetch("http://localhost:8000/contact", {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      alert("Message Sent Successfully");
+
+      setFormData({
+        fullName: "",
+        email: "",
+        phone: "",
+        message: "",
+      });
+
+    } catch (error) {
+
+      console.log(error);
+
+      alert("Something went wrong");
+    }
+  };
+
   return (
     <div className={styles.formWrapper}>
 
       <h2 className={styles.formTitle}>Send us a message</h2>
 
-      <form className="d-flex flex-column gap-3">
+      <form onSubmit={handleSubmit} className="d-flex flex-column gap-3">
 
         {/* Name */}
         <input
           type="text"
+          name="fullName"
           placeholder="Full Name"
-          className={`form-control ${styles.input}`}
+          value={formData.fullName}
+          onChange={handleChange}
+          className={`form-control ${styles.contactInput}`}
         />
 
         {/* Email */}
         <input
           type="email"
+          name="email"
           placeholder="Email Address"
-          className={`form-control ${styles.input}`}
+          value={formData.email}
+          onChange={handleChange}
+          className={`form-control ${styles.contactInput}`}
         />
 
         {/* Phone */}
         <input
           type="text"
+          name="phone"
           placeholder="Phone Number"
-          className={`form-control ${styles.input}`}
+          value={formData.phone}
+          onChange={handleChange}
+          className={`form-control ${styles.contactInput}`}
         />
 
         {/* Message */}
         <textarea
-          rows="5"
+          name="message"
           placeholder="Your Message"
-          className={`form-control ${styles.textarea}`}
+          value={formData.message}
+          onChange={handleChange}
+          className={`form-control ${styles.contactTextarea}`}
         />
 
         {/* Button */}
--- Server/index.js
+++ Server/index.js
@@ -10,7 +10,7 @@
 
     app.use(
     cors({
-        origin: "http://localhost:5143",
+        origin: "http://localhost:5173",
     }),
     );
 
